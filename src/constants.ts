f7123b2087ead2d600eb4a2e01ce723d17b0eab9
Alex Wang <alexwang.maker@gmail.com>
2025-11-25 01:28:07 +0800

    chore: test release
    Release-As: 0.2.1

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,4 @@
 export const BLOCK_EQUATION_NAME = 'block_equation'
 export const INLINE_EQUATION_NAME = 'inline_equation'
 export const REQUEST_OPEN_EDITOR_KEY = 'shouldOpenEquationEditor'
+export const a = 1
